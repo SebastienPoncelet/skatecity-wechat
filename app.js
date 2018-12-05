acd633713fc2087239959469259af0bfea043cce
Sophie Henry <sophiesophiehenry@googlemail.com>
2018-12-05 15:57:04 +0800

    fixed space between map and description on show page

--- app.js
+++ app.js
@@ -44,8 +44,8 @@ App({
   },
   globalData: {
 
-    // host: "https://skatecity.wogengapp.cn/",
-  host: 'http://localhost:3000/',
+    host: "https://skatecity.wogengapp.cn/",
+  // host: 'http://localhost:3000/',
 
 
 
