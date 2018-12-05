d0806caecb95a58ae45b18d5878b306375b40b40
Brian Thomas <brian@vertwebsolutions.com>
2018-12-05 14:36:13 +0800

    preview image to be fixed

--- pages/show/show.js
+++ pages/show/show.js
@@ -146,6 +146,13 @@ Page({
     spot: { }
 
   },
+  openImage: function () {
+    console.log('running');
+    wx.previewImage({
+      current: 'http://115.28.2.176/skate/1.jpg',
+      urls: ['http://115.28.2.176/skate/3.jpg', 'http://115.28.2.176/skate/4.jpg']
+    })
+  },
 
 
   /**
